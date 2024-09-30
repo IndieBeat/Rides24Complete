149bc755291eb346b5de6e381e25b2b7af56482c
762076 <762076@u025354.gi.ehu.es>
2024-09-30 16:57:04 +0200

    Grafo de flujo y estructura basica de tests de BookRide

---
+++ src/test/java/BookRideWhiteTest.java
@@ -0,0 +1,18 @@
+
+import domain.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class BookRideWhiteTest {
+
+	
+	@Test
+	public void test1() {
+		
+	}
+}
