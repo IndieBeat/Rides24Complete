418f12b937acd8dcc136c27988bafd2537322c20
Ramon <ramoncasadofranco@hotmail.es>
2024-10-04 01:25:02 +0200

    Caja blanca tests anotados con limpieza de datos

--- src/test/java/BookRideWhiteTest.java
+++ src/test/java/BookRideWhiteTest.java
@@ -22,177 +22,258 @@ public class BookRideWhiteTest {
 	
 	@Test
 	public void test1NoUser() {
-		try {
-			sut.open();
+		String username="Ray";
+		String driverName="Test1Driver";
+		String rideFrom="Murcia";
+		String rideTo="Barcelona";
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(2024, Calendar.MAY, 30);
+		Date date = UtilDate.trim(cal.getTime());
 
-			String username="Ray";
-			
+		try {
 			testDA.open();
-			Calendar cal = Calendar.getInstance();
-			cal.set(2024, Calendar.MAY, 30);
-			Date date2 = UtilDate.trim(cal.getTime());
-			Driver driver=testDA.addDriverWithRide("Test","Madrid", "Barcelona", date2, 5, 2);
-			
-			Ride ride=driver.getCreatedRides().get(0);
+			Driver driver=testDA.addDriverWithRide(driverName,rideFrom,rideTo, date, 5, 2);
 			testDA.close();
+			Ride ride=driver.getCreatedRides().get(0);
 			
+			sut.open();
 			int seats=2;
 			double desk=0;
 			boolean booked=sut.bookRide(username, ride, seats, desk);
+			sut.close();
 			
 			assertTrue(!booked);
 		} catch (Exception e) {
 			fail();
+		}finally {
+			testDA.open();
+			
+			testDA.removeRide(driverName, rideFrom, rideTo, date);
+			testDA.removeDriver(driverName);
+			
+			testDA.close();
 		}
 	}
 	
 	@Test
 	public void test2NoSeats() {
+		String username="Traveler";
+		String driverName="TestDriver";
+		String rideFrom="Murcia";
+		String rideTo="Barcelona";
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(2024, Calendar.MAY, 30);
+		Date date = UtilDate.trim(cal.getTime());
 		try {
-			sut.open();
-
-			String username="Unax";
-			
 			testDA.open();
-			Calendar cal = Calendar.getInstance();
-			cal.set(2024, Calendar.MAY, 30);
-			Date date2 = UtilDate.trim(cal.getTime());
-			Driver driver=testDA.addDriverWithRide("Test","Madrid", "Barcelona", date2, 5, 2);
-			
-			Ride ride=driver.getCreatedRides().get(0);
+			Driver driver=testDA.addDriverWithRide(driverName,rideFrom,rideTo, date, 5, 2);
+			testDA.createTraveler(username, "123", 10);
 			testDA.close();
 			
+			Ride ride=driver.getCreatedRides().get(0);
+			
+			sut.open();
 			int seats=200;
 			double desk=0;
 			boolean booked=sut.bookRide(username, ride, seats, desk);
+			sut.close();
 			
 			assertTrue(!booked);
 		} catch (Exception e) {
 			fail();
+		}finally {
+			testDA.open();
+			
+			testDA.removeRide(driverName, rideFrom, rideTo, date);
+			testDA.removeDriver(driverName);
+			testDA.removeTraveler(username);
+			
+			testDA.close();
 		}
 	}
 
 	@Test
 	public void test3NoRider() {
+		String username="Traveler";
+		
 		try {
-			sut.open();
-
-			String username="Unax";
 			Ride ride=null;
+			testDA.open();
+			testDA.createTraveler(username, "123", 10);
+			testDA.close();
+			
+			sut.open();
 			int seats=2;
 			double desk=0;
 			boolean booked=sut.bookRide(username, ride, seats, desk);
+			sut.close();
 			
-			System.out.println(booked);
 			assertTrue(!booked);
+		} catch(NullPointerException e) {
+			fail();
 		} catch (Exception e) {
 			fail();
+		}finally {
+			testDA.open();
+			testDA.removeTraveler(username);
+			testDA.close();
 		}
 	}
 	
 	@Test
 	public void test4NoMoney() {
-		try {
-			sut.open();
+		String username="Traveler";
+		String driverName="TestDriver";
+		String rideFrom="Murcia";
+		String rideTo="Barcelona";
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(2024, Calendar.MAY, 30);
+		Date date = UtilDate.trim(cal.getTime());
 
-			String username="Luken";
-			
+		try {
 			testDA.open();
-			Calendar cal = Calendar.getInstance();
-			cal.set(2024, Calendar.MAY, 30);
-			Date date2 = UtilDate.trim(cal.getTime());
-			Driver driver=testDA.addDriverWithRide("Test","Madrid", "Barcelona", date2, 5, 200);
-			
-			Ride ride=driver.getCreatedRides().get(0);
+			testDA.createTraveler(username, "123", 10);
+			Driver driver=testDA.addDriverWithRide(driverName,rideFrom, rideTo, date, 5, 200);
 			testDA.close();
 			
+			Ride ride=driver.getCreatedRides().get(0);
+			
+			sut.open();
 			int seats=4;
 			double desk=0;
 			boolean booked=sut.bookRide(username, ride, seats, desk);
+			sut.close();
 			
 			assertTrue(!booked);
 		} catch (Exception e) {
 			fail();
+		}finally {
+			testDA.open();
+			
+			testDA.removeBookings(username);
+			testDA.removeTraveler(username);
+			testDA.removeRide(driverName, rideFrom, rideTo, date);
+			testDA.removeDriver(driverName);
+			
+			testDA.close();
 		}
 	}
 	
 	@Test
 	public void test5OK() {
+		String travelerName="TestTraveler";
+		String driverName="TestDriver";
+		String rideFrom="Murcia";
+		String rideTo="Barcelona";
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(2024, Calendar.MAY, 30);
+		Date date = UtilDate.trim(cal.getTime());
+		
 		try {
-			sut.open();
-
-			String username="Unax";
-			
 			testDA.open();
-			Calendar cal = Calendar.getInstance();
-			cal.set(2024, Calendar.MAY, 30);
-			Date date2 = UtilDate.trim(cal.getTime());
-			Driver driver=testDA.addDriverWithRide("Test","Madrid", "Barcelona", date2, 5, 2);
-			
-			Ride ride=driver.getCreatedRides().get(0);
+			Driver driver=testDA.addDriverWithRide(driverName, rideFrom, rideTo, date, 5, 2);
+			Traveler t=testDA.createTraveler(travelerName, "123", 10);
 			testDA.close();
 			
+			Ride ride=driver.getCreatedRides().get(0);
+			
+			
+			sut.open();
 			int seats=2;
 			double desk=0;
-			boolean booked=sut.bookRide(username, ride, seats, desk);
+			boolean booked=sut.bookRide(t.getUsername(), ride, seats, desk);
+			sut.close();
 			
 			assertTrue(booked);
 		} catch (Exception e) {
+			e.printStackTrace();
 			fail();
+		} finally {
+			testDA.open();
+			
+			testDA.removeBookings(travelerName);
+			testDA.removeTraveler(travelerName);
+			testDA.removeRide(driverName, rideFrom, rideTo, date);
+			testDA.removeDriver(driverName);
+			
+			testDA.close();
 		}
 	}
-	
+
 	@Test
 	public void test6UltraDesk() {
+		String username="Traveler";
+		String driverName="Driver";
+		String rideFrom="Murcia";
+		String rideTo="Barcelona";
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(2024, Calendar.MAY, 30);
+		Date date = UtilDate.trim(cal.getTime());
 		try {
 			sut.open();
-
-			String username="Unax";
-			
 			testDA.open();
-			Calendar cal = Calendar.getInstance();
-			cal.set(2024, Calendar.MAY, 30);
-			Date date2 = UtilDate.trim(cal.getTime());
-			Driver driver=testDA.addDriverWithRide("Test","Madrid", "Barcelona", date2, 5, 2);
+			
+			Driver driver=testDA.addDriverWithRide(driverName,rideFrom, rideTo, date, 5, 2);
 			
 			Ride ride=driver.getCreatedRides().get(0);
-			testDA.close();
+
+			Traveler t=testDA.createTraveler(username, "123", 10);
 			
 			int seats=2;
 			double desk=200;
 			boolean booked=sut.bookRide(username, ride, seats, desk);
-			
+
+			sut.close();
 			assertTrue(!booked);
 		} catch (Exception e) {
-			assertTrue(true);
+			e.printStackTrace();
+			fail();
 		}finally {
+			testDA.open();
 			
+			testDA.removeBookings(username);
+			testDA.removeTraveler(username);
+			testDA.removeRide(driverName, rideFrom, rideTo, date);
+			testDA.removeDriver(driverName);
+			
+			testDA.close();
 		}
 	}
-	
+	/*
 	@Test
 	public void test7DriverNoTraveler() {
 		try {
 			sut.open();
-
-			String username="Urtzi";
-			
 			testDA.open();
+			
 			Calendar cal = Calendar.getInstance();
 			cal.set(2024, Calendar.MAY, 30);
 			Date date2 = UtilDate.trim(cal.getTime());
-			Driver driver=testDA.addDriverWithRide("Test","Madrid", "Barcelona", date2, 5, 2);
+			Driver driver=testDA.addDriverWithRide("Test","Murcia", "Barcelona", date2, 5, 2);
 			
 			Ride ride=driver.getCreatedRides().get(0);
-			testDA.close();
 			
 			int seats=2;
 			double desk=200;
-			boolean booked=sut.bookRide(username, ride, seats, desk);
+			boolean booked=sut.bookRide(driver.getUsername(), ride, seats, desk);
 			
-			assertTrue(booked);
+			testDA.removeBookings(driver.getUsername());
+			testDA.removeRide(driver.getUsername(), ride.getFrom(), ride.getTo(), ride.getDate());
+			testDA.removeDriver(driver.getUsername());
+			
+			testDA.close();
+			sut.close();
+			assertTrue(!booked);
 		} catch (Exception e) {
 			fail();
+		}finally {
+			testDA.close();
+			sut.close();
 		}
-	}
+	}*/
 }
--- src/test/java/testOperations/TestDataAccess.java
+++ src/test/java/testOperations/TestDataAccess.java
@@ -2,6 +2,7 @@ package testOperations;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.persistence.EntityManager;
@@ -9,8 +10,10 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import configuration.ConfigXML;
+import domain.Booking;
 import domain.Driver;
 import domain.Ride;
+import domain.Traveler;
 
 
 public class TestDataAccess {
@@ -19,19 +22,12 @@ public class TestDataAccess {
 
 	ConfigXML  c=ConfigXML.getInstance();
 
-
 	public TestDataAccess()  {
-		
 		System.out.println("TestDataAccess created");
-
 		//open();
-		
 	}
 
-	
 	public void open(){
-		
-
 		String fileName=c.getDbFilename();
 		
 		if (c.isDatabaseLocal()) {
@@ -47,9 +43,8 @@ public class TestDataAccess {
 			  db = emf.createEntityManager();
     	   }
 		System.out.println("TestDataAccess opened");
-
-		
 	}
+	
 	public void close(){
 		db.close();
 		System.out.println("TestDataAccess closed");
@@ -66,74 +61,128 @@ public class TestDataAccess {
 		} else 
 		return false;
     }
+	
 	public Driver createDriver(String name, String pass) {
 		System.out.println(">> TestDataAccess: addDriver");
 		Driver driver=null;
-			db.getTransaction().begin();
-			try {
-			    driver=new Driver(name,pass);
-				db.persist(driver);
-				db.getTransaction().commit();
-			}
-			catch (Exception e){
-				e.printStackTrace();
-			}
-			return driver;
+		db.getTransaction().begin();
+		try {
+		    driver=new Driver(name,pass);
+			db.persist(driver);
+			db.getTransaction().commit();
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		return driver;
     }
+	
 	public boolean existDriver(String email) {
 		 return  db.find(Driver.class, email)!=null;
 		 
 
 	}
 		
-		public Driver addDriverWithRide(String name, String from, String to,  Date date, int nPlaces, float price) {
-			System.out.println(">> TestDataAccess: addDriverWithRide");
-				Driver driver=null;
-				db.getTransaction().begin();
-				try {
-					 driver = db.find(Driver.class, name);
-					if (driver==null) {
-						System.out.println("Entra en null");
-						driver=new Driver(name,null);
-				    	db.persist(driver);
-					}
-				    driver.addRide(from, to, date, nPlaces, price);
-					db.getTransaction().commit();
-					System.out.println("Driver created "+driver);
-					
-					return driver;
-					
-				}
-				catch (Exception e){
-					e.printStackTrace();
-				}
-				return null;
-	    }
+	public Driver addDriverWithRide(String name, String from, String to,  Date date, int nPlaces, float price) {
+		System.out.println(">> TestDataAccess: addDriverWithRide");
+		Driver driver=null;
+		db.getTransaction().begin();
+		try {
+			 driver = db.find(Driver.class, name);
+			if (driver==null) {
+				System.out.println("Entra en null");
+				driver=new Driver(name,null);
+		    	db.persist(driver);
+			}
+		    driver.addRide(from, to, date, nPlaces, price);
+			db.getTransaction().commit();
+			System.out.println("Driver created "+driver);
+			
+			return driver;
+			
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		return null;
+    }
+	
+	
+	public boolean existRide(String name, String from, String to, Date date) {
+		System.out.println(">> TestDataAccess: existRide");
+		Driver d = db.find(Driver.class, name);
+		if (d!=null) {
+			return d.doesRideExists(from, to, date);
+		} else 
+		return false;
+	}
+	
+	public Ride removeRide(String name, String from, String to, Date date ) {
+		System.out.println(">> TestDataAccess: removeRide");
+		Driver d = db.find(Driver.class, name);
+		if (d!=null) {
+			db.getTransaction().begin();
+			Ride r= d.removeRide(from, to, date);
+			db.getTransaction().commit();
+			System.out.println("created rides" +d.getCreatedRides());
+			return r;
+
+		} else 
+		return null;
+
+	}
+
+	public Traveler createTraveler(String username,String pass,double money) {
+		Traveler traveler = null;
 		
-		
-		public boolean existRide(String name, String from, String to, Date date) {
-			System.out.println(">> TestDataAccess: existRide");
-			Driver d = db.find(Driver.class, name);
-			if (d!=null) {
-				return d.doesRideExists(from, to, date);
-			} else 
+		db.getTransaction().begin();
+		try {
+			traveler = db.find(Traveler.class, username);
+			if (traveler==null) {
+				System.out.println("Entra en null");
+				traveler = new Traveler(username, pass);
+				traveler.setIzoztatutakoDirua(0);
+				traveler.setMoney(money);
+				traveler.setBalorazioa(14);
+				traveler.setBalkop(4);
+		    	db.persist(traveler);
+
+				db.getTransaction().commit();
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return traveler;
+	}
+	
+	public boolean removeTraveler(String username) {
+		db.getTransaction().begin();
+
+		try {
+			Traveler t = db.find(Traveler.class, username);
+			if (t==null) return false;
+			
+			db.remove(t);
+			db.getTransaction().commit();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+	
+	public boolean removeBookings(String username) {
+		Traveler t=db.find(Traveler.class, username);
+		List<Booking> list=t.getBookedRides();
+		db.getTransaction().begin();
+		try {
+			for(Booking b:list) {
+				db.remove(b);
+			}
+			db.getTransaction().commit();
+		}catch(Exception e) {
+			e.printStackTrace();
 			return false;
 		}
-		public Ride removeRide(String name, String from, String to, Date date ) {
-			System.out.println(">> TestDataAccess: removeRide");
-			Driver d = db.find(Driver.class, name);
-			if (d!=null) {
-				db.getTransaction().begin();
-				Ride r= d.removeRide(from, to, date);
-				db.getTransaction().commit();
-				System.out.println("created rides" +d.getCreatedRides());
-				return r;
-
-			} else 
-			return null;
-
-		}
-
-
-		
+		return true;
+	}
 }
